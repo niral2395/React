c31bb6e505fa61dcfc1a944d5649f2a3ab96a6fa
niral2395 <niralshah2395@gmail.com>
2019-01-09 14:31:16 -0800

    TodoList

---
+++ TodoList/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Todolist from "./TodoList";
+
+var destination = document.querySelector("#container");
+
+ReactDOM.render(
+	
+	<div>
+		<Todolist/>
+	</div>,
+	destination	
+
+);
